e30c42d545977812fc9bb84065c321816e7fdd73
Juho Erik Sippola <s1siju01@students.osao.fi>
2022-04-05 12:19:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function displayHello() {
+    let test = document.getElementById('greeting').innerHTML;
+    if (test == "")
+    {
+        document.getElementById('greeting').innerHTML = "Hello World!"
+    }
+    else
+    {
+        document.getElementById('greeting').innerHTML = "";
+    }
+}
